200de9ae3b8ea6c37aae751e6fb5d164d0f63870
mvpetri <mvpetri@gmail.com>
2021-06-24 20:02:38 -0300

    removendo espaço após cat_id

--- py/arxiv_taxonomy.py
+++ py/arxiv_taxonomy.py
@@ -23,7 +23,7 @@ for group in groups:
     for h4, p in zip(group[1].find_all("h4"), group[1].find_all("p")):
         cat_name = h4.find("span")
         cat_name.extract()
-        cat_id = h4.text
+        cat_id = h4.text.strip()
         cat_description = p.text
         arxiv_taxonomy[cat_id] = {
                 "group_name":group_name,
